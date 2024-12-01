509defebbba17f068eed43435022105a5249d8a3
Jakub Grabarski <kubagosthmsg@gmail.com>
2024-12-01 11:18:48 +0100

    initialize Vue.js project with basic configuration and OpenMap component

---
+++ immersihome/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <OpenMap />
+</template>
+
+<script>
+import OpenMap from './components/OpenMap.vue';
+
+export default {
+  name: 'App',
+  components: {
+    OpenMap
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ immersihome/src/components/OpenMap.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="map" style="width: 100%; height: 400px;"></div>
+</template>
+
+<script>
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default {
+    name: "OpenMap",
+    data() {
+        return {
+            map: null,
+        };
+    },
+    mounted() {
+        this.map = L.map("map").setView([55.0152622, 11.9150684], 13);
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(this.map);
+
+        L.marker([55.0152622, 11.9150684])
+            .addTo(this.map)
+            .bindPopup("Home Sweet Home")
+            .openPopup();
+    },
+};
+</script>
+
+<style>
+#map {
+  height: 100vh !important; 
+  width: 100vw !important; 
+}
+</style>
